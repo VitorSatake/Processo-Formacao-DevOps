54f111aef124b975ed94f2dce75f2aa5d35b1d51
Vitor <vitor.scastro2013@hotmail.com>
2024-04-22 16:35:23 -0300

    novo

--- devops-terraform/curso-alura-iac/infra/ECS.tf
+++ devops-terraform/curso-alura-iac/infra/ECS.tf
@@ -46,3 +46,26 @@ resource "aws_ecs_task_definition" "api" {
 ]
 )
 }
+
+resource "aws_ecs_service" "api" {
+  name            = "api"
+  cluster         = module.ecs.ecs_cluster_id
+  task_definition = aws_ecs_task_definition.api.arn
+  desired_count   = 3
+
+  load_balancer {
+    target_group_arn = aws_lb_target_group.alvo.arn
+    container_name   = "variavel"
+    container_port   = 8000
+  }
+
+  network_configuration {
+    subnets = module.vpc.private_subnets
+    security_groups = [aws_security_group.privado.id]
+  }
+
+  capacity_provider_strategy {
+    capacity_provider = "FARGATE"
+    weight = 1
+  }
+}
